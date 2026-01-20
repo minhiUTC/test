95a9531465b30b21afab6ecc5fe7d221d02be968
cow-CNTT-UTC <maithiyennhi1809@gmail.com>
2026-01-20 17:00:32 +0700

    update final

--- WEBTESTK65-master/Form validation/validator.js
+++ WEBTESTK65-master/Form validation/validator.js
@@ -34,3 +34,46 @@ function checkEmpty(val, elm) {
 }
 /* Không chỉnh sửa bất kì đoạn code nào ở trên */
 /* Code từ đây */
+const emailInput = document.querySelector("#input__email");
+const passwordInput = document.querySelector("#input__password");
+const passwordConfirmInput = document.querySelector("#input__password-again");
+const provinceInput = document.querySelector("#province");
+const genderInputs = document.querySelectorAll('input[name="gender"]');
+
+const fields = [nameInput, emailInput, passwordInput, passwordConfirmInput, provinceInput];
+
+fields.forEach(input => {
+    input.oninput = () => {
+        toggleMessage(true, input);
+    }
+});
+
+[emailInput, passwordInput, passwordConfirmInput, provinceInput].forEach(input => {
+    input.onblur = () => {
+        checkEmpty(input.value, input);
+    }
+});
+
+document.addEventListener('click', (e) => {
+    [emailInput, passwordInput, passwordConfirmInput, provinceInput].forEach(input => {
+        checkEmpty(input.value, input);
+    });
+
+    let genderCheck = Array.from(genderInputs).some(radio => radio.checked);
+    let containerGender = document.querySelector('input[name="gender"]').closest('.input-form');
+    
+    if ( genderCheck) {
+        containerGender.classList.add("invalid");
+        containerGender.querySelector('.form-message').textContent = "Vui lòng chọn giới tính!";
+    } else {
+        containerGender.classList.remove("invalid");
+        containerGender.querySelector('.form-message').textContent = "";
+    }
+});
+
+genderInputs.forEach(radio => {
+    radio.onchange = () => {
+        const containerGender = radio.closest('.input-form');
+        containerGender.classList.remove("invalid");
+    }
+});
